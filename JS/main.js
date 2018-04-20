13db568b222efdb793291319fe935082093240d4
caro <carolinegaudiard@gmail.com>
2018-04-20 16:30:59 +0100

    avancement projet

--- JS/main.js
+++ JS/main.js
@@ -1,71 +1,140 @@
 	// JS to open and close sidebar overlay effect 
 
 	function w3_open() {
-	 document.getElementById("mySidebar").style.display = "block";
-	  document.getElementById("myOverlay").style.display = "block";
+		document.getElementById("mySidebar").style.display = "block";
+		// document.getElementById("myOverlay").style.display = "block";
 	}
 
 	function w3_close() {
-	  document.getElementById("mySidebar").style.display = "none";
-	  document.getElementById("myOverlay").style.display = "none";
+		document.getElementById("mySidebar").style.display = "none";
+		// document.getElementById("myOverlay").style.display = "none";
 	}
 
+	// pour gerer le menu lorsqu on change 'd'onglet'
+	function openView(pageId) {
+		$('.view').hide();
+		$('#' + pageId).show();
+		w3_close();
+	}
 
-	// Deezer
-(function (){
-    'use strict';
-	$(document).ready(function(){
+	function minToHour(minutes) {
+		//On test que minutes est bien un nombre entier
+		var Myexp = new RegExp("^[0-9]+$", "g");
+		if (Myexp.test(minutes)) {
+			var nbHour = parseInt(minutes / 60);
+			var nbminuteRestante = (minutes % 60);
+			if (nbminuteRestante == 0) {
+				alert(nbHour + "h");
+			} else {
+				alert(nbHour + "h" + nbminuteRestante);
+			}
+		}
+	}
 
-// Faire apparaitre dans la page apres avoir taper dans la bar de recherche
-		
-		$('#boutonrechercher').on('click', function(event){
-			if ($('select#trimusique').val() === $("option:contains('Album')")[0].value && $('#barrecherche').val() != null){
-				
-				var TabApi = [];
+	function consulterAlbum(resultat) {
+
+		return function (event) {
+
+			// var url = window.location.href.indexOf('album/');
+			// alert(url);
+			var id = resultat.album.id;
+			console.log(id);
+
+			$.ajax({
+				url: 'https://api.deezer.com/album/' + id + '/tracks',
+				data: {
+					output: 'jsonp',
+				},
+				dataType: 'jsonp',
+			}).then(function (response) {
+				openView('pagealbum');
+				$('#albumNom').html(resultat.album.title);
+
+				console.log('album', response);
+				// affichageElement(response.data);
+			}); /* Accolade fin de function(response){} */
+		};
+	}
+
+	function affichageElements(data) {
+		console.log(data.length);
+		var sortie = $('div.resultats');
+		sortie.empty();
+
+		for (var i = 0; i < data.length; i++) {
+
+			var idAlbum = data[i].album.id;
+			var consulterAlbumBoutonId = 'consulterAlbumBouton' + i;
+			var div = $('<div class="container-artist"></div>');
+			sortie.append(div);
+
+			// console.log(div)
+
+			div.append('<div class="container"><div id="numeroArtiste"><p>Album N°</p>' +
+				i + '</div>');
+
+			div.append('<div class="album-img"><img src="' + data[i].album.cover_medium +
+					'"></div>')
+				.append('<h2>' + data[i].artist.name + ' / Album : ' + data[i].album.title + ' / Duree : ' +
+					data[i].duration + ' minutes </h2>')
+				.append('<div class="container-info-artiste flex">' +
+					'<button class="w3-bar-item w3-button w3-black">' +
+					'écouter un extrait' +
+					'</button>' +
+					'<button id="' + consulterAlbumBoutonId + '" class="w3-bar-item w3-button w3-black consulterAlbumBouton">' +
+					'Consulter Album' +
+					'</button>' +
+					'<button class="w3-bar-item w3-button w3-black">' +
+					'Voir la fiche de lartiste' +
+					'</button>' +
+					'</div></div>');
+
+			$(document).on('click', '#' + consulterAlbumBoutonId, consulterAlbum(data[i]));
+
+		} /* Boucle for */
+	}
+
+	(function () {
+		'use strict';
+		$(document).ready(function () {
+
+			if (window.history && window.history.pushState) {
+				window.history.pushState('forward', null, './#forward');
+				$(window).on('popstate', function () {
+					alert('Back button was pressed.');
+				});
+			}
+
+			openView('pagerecherche');
+
+			// Faire apparaitre dans la page apres avoir taper dans la bar de recherche
+			var albumOption = $("option:contains('Album')")[0].value;
+			var artisteOption = $("option:contains('Artiste')")[0].value;
+
+			$('#boutonrechercher').on('click', function (event) {
 				var barrecherche = $('#barrecherche').val();
-				
-				$.ajax({
-					 url : 'https://api.deezer.com/search?q=' + barrecherche + '&order=TRACK_ASC',
-  					 data : {
-  					 	output : 'jsonp',
-  					 },
-  					 dataType : 'jsonp',
-  					}).then(function(data){
+				if (barrecherche) {
 
-  					TabApi = data;
+					$.ajax({
+						url: 'https://api.deezer.com/search?q=' + barrecherche + '&order=TRACK_ASC',
+						data: {
+							output: 'jsonp',
+						},
+						dataType: 'jsonp',
+					}).then(function (response) {
+						console.log(response);
 
-  					console.log(TabApi.data);
-
-  					  for (var i = 0; i < TabApi.data.length; i++){
-  					 	$('div.container-artist').append('<div class="w3-container"><div id="numeroArtiste"><p>Album N°</>'+ i + '</div>');
-  					 	$('div.container-artist').append('<div class="album-img"><img src="'+ TabApi.data[i].album.cover_medium +'"></div>')
-  					 	.append('<h2>'+ TabApi.data[i].artist.name + ' / Album : ' + TabApi.data[i].album.title + ' / Duree : ' + TabApi.data[i].duration + ' minutes </h2>')
-                   		.append('<div class="container-info-artiste flex"><button class="w3-bar-item w3-button w3-black">écouter un extrait</button><button class="w3-bar-item w3-button w3-black">Consulter Album</button><button class="w3-bar-item w3-button w3-black">Voir la fiche de lartiste</button></div></div>');
-                   	}
-
-              });
-            }
-        });
-    });
-})();
-
-
-  /*else if($('select#trimusique').val() === $("option:contains('Artiste')")[0].value && $('#barrecherche').val() != null){
-        
-        $.ajax({
-           url : 'https://api.deezer.com/search?q=' + barrecherche ,
-             data : {
-              output : 'jsonp',
-             },
-             dataType : 'jsonp',
-             }).then(function(data){
-
-             for (var i = 0; i < TabApi.data.length; i++){
-              $('div.container-artist').append('<div class="w3-container"><div id="numeroArtiste"><p>Album N°</>'+ i + '</div>');
-              $('div.container-artist').append('<div class="album-img"><img src="'+ TabApi.data[i].album.cover_medium +'"></div>')
-              .append('<h2>'+ TabApi.data[i].artist.name + ' / Album : ' + TabApi.data[i].album.title + ' / Duree : ' + TabApi.data[i].duration + ' minutes </h2>')
-                      .append('<div class="container-info-artiste flex"><button class="w3-bar-item w3-button w3-black">écouter un extrait</button><button class="w3-bar-item w3-button w3-black">Consulter Album</button><button class="w3-bar-item w3-button w3-black">Voir la fiche de lartiste</button></div></div>');
-                        }
-                        else {
-
-                       alert("salut"); */
+						if ($('select#trimusique').val() === albumOption) {
+							affichageElements(response.data);
+						} else if ($('select#trimusique').val() === artisteOption) {
+							affichageElements(response.data);
+						} else {
+							alert("salut");
+						}
+					}); /* Accolade fin de function(response){} */
+				} else {
+					console.log('Il n y a rien dans la barre de recherche');
+				}
+			});
+		});
+	})();
